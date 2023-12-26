831812a22e66302ffacd565bd55615969d28a1a6
diginieit <rafin.hossain.8990@gmail.com>
2023-12-26 13:06:06 +0600

    update contact page

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,14 +1,24 @@
 import Banner from "@/components/ContactPage/Banner";
-
+import ContactForm from "@/components/ContactPage/ContactForm";
+import ContactIntro from "@/components/ContactPage/ContactIntro";
+import SideLatestPost from "@/components/SideMenu/LatestPost";
 
 const Contact = () => {
-    return (
-        <div>
-            <Banner/>
-            
-            
+  return (
+    <div>
+      <Banner />
+
+      <section className="md:grid md:grid-cols-3">
+        <div className="md:col-span-2">
+          <ContactIntro />
+          <ContactForm />
         </div>
-    );
+        <div className="md:col-span-1 w-full px-4 ">
+          <SideLatestPost />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Contact;
